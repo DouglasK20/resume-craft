94e8b233be6c2318a7135f3ea1d8f000088f5f0d
Douglas Kunzler <do89161@gmail.com>
2025-01-30 13:10:39 -0300

    Mandando embora.

---
+++ src/components/pages/dashboard/resume/structure-sidebar/sections/templates-list.tsx
@@ -0,0 +1,11 @@
+import { LayoutTemplate } from "lucide-react";
+import { SectionTitle } from "../../infos-sidebar/section-title";
+
+
+export function TemplatesListSection() {
+    return (
+        <div>
+            <SectionTitle title="Modelos" icon={LayoutTemplate} />
+        </div>
+    );
+}
